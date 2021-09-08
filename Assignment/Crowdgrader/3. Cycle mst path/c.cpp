566b2ded745be80e2d0ecd1397795608879abd01
imranZMiko <imran.zh.miko@gmail.com>
2021-09-09 00:20:17 +0600

    Revert "Added named algorithms"
    This reverts commit dbfc5e4d9233ad40544e4ce157a9bca796f6ec73.

--- Assignment/Crowdgrader/3. Cycle mst path/c.cpp
+++ Assignment/Crowdgrader/3. Cycle mst path/c.cpp
@@ -1,6 +1,7 @@
 #include<bits/stdc++.h>
 using namespace std;
-
+#define LOOP3 for(k=0;k<n;k++) for(i=0;i<n;i++) for(j=0;j<n;j++) 
+#define LOOP2 for(i=0;i<n;i++) for(j=0;j<n;j++)  
 unsigned long long path_count[1000][1000];
 int n,m;
 
@@ -21,20 +22,17 @@ void input()
 void floyd()
 {
     int i,j,k;
-    for(k=0;k<n;k++) 
-        for(i=0;i<n;i++) 
-            for(j=0;j<n;j++) 
-                path_count[i][j]+=path_count[i][k]*path_count[k][j];
+    LOOP3 
+        path_count[i][j]+=path_count[i][k]*path_count[k][j];
 }
 
 void output()
 {
     int i,j;
     floyd();
-    for(i=0;i<n;i++)
-        for(j=0;j<n;j++)
-            if(i!=j)
-                printf("Number of paths from %d to %d: %llu\n", i, j, path_count[i][j]);
+    LOOP2
+        if(i!=j)
+            printf("Number of paths from %d to %d: %llu\n", i, j, path_count[i][j]);
         
 }
 
--- floyd warshall/floyd.cpp
+++ floyd warshall/floyd.cpp
@@ -9,10 +9,7 @@ void initialize()
 {
 	int i,j;
 	memset(dis,0,sizeof(dis));
-	for(i=0;i<n;i++) 
-		for(j=0;j<n;j++) 
-			if(i!=j) 
-				dis[i][j]=INF;
+	LOOP2 if(i!=j) dis[i][j]=INF;
 			
 }
 
@@ -42,11 +39,8 @@ void input()
 void floyd_warshall()
 {
 	int i, j, k;
-	for(k=0;k<n;k++) 
-		for(i=0;i<n;i++) 
-			for(j=0;j<n;j++) 
-				if(dis[i][k]+dis[k][j]<dis[i][j])
-					dis[i][j]=dis[i][k]+dis[k][j];
+	LOOP3 if(dis[i][k]+dis[k][j]<dis[i][j])
+			dis[i][j]=dis[i][k]+dis[k][j];
 }
 	
 void printPath()
